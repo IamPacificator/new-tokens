af14f29d93cf1eef3ef88afe263d01968e5bcf6f
IamPacificator <155380381+IamPacificator@users.noreply.github.com>
2024-01-01 22:11:51 +0400

    webhook.ts

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -1,36 +1,47 @@
-import fetch from 'node-fetch';
-
-const rpc = `https://rpc.helius.xyz/?api-key=${process.env.HELIUS_KEY}`;
-
-const getAsset = async (token: string) => {
-  const response = await fetch(rpc, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      jsonrpc: '2.0',
-      id: 'my-id',
-      method: 'getAsset',
-      params: {
-        id: token,
-      },
-    }),
-  });
-  const { result } = await response.json();
-  return result;
-};
-
 export default async function handler(req: any, res: any) {
   try {
     if (req.method === 'POST') {
       const webhook: any = process.env.DISCORD_WEBHOOK;
+      const webhookData = req.body[0]; // Assuming the payload is an array with a single object
 
-      let webhookData = req.body;
+      const tokenMintDetails = webhookData.tokenTransfers[0]; // Assuming there's only one token transfer
 
-      console.log(webhookData, 'e1');
-      console.log(webhookData[0].events.tokenTransfers[0]);
-      let token: any = await getAsset(webhookData[0].events.tokenTransfers[0].token);
+      // Assuming you have a function to get asset details based on the minted token
+      const tokenDetails = await getAsset(tokenMintDetails.token);
+
+      const embed = {
+        title: `${tokenDetails.content.metadata.name} has been minted!`,
+        url: `https://solscan.io/token/${tokenMintDetails.token}`,
+        color: 16486972,
+        fields: [
+          { name: '\u200B', value: '\u200B' },
+          {
+            name: ':gem: Minted Amount',
+            value: `**${webhookData.accountData.length} Tokens**`,
+            inline: true,
+          },
+          {
+            name: ':date: Mint Date',
+            value: `<t:${webhookData.timestamp}:R>`,
+            inline: true,
+          },
+          { name: '\u200B', value: '\u200B' },
+          {
+            name: 'Minting Account',
+            value: `${webhookData.feePayer.slice(0, 4)}..${webhookData.feePayer.slice(-4)}`,
+            inline: true,
+          },
+        ],
+        image: {
+          url: tokenDetails.content.files[0].uri,
+        },
+        timestamp: new Date().toISOString(),
+        footer: {
+          text: 'Helius',
+          icon_url:
+            'https://assets-global.website-files.com/641a8c4cac3aee8bd266fd58/642b5b2804ea37191a59737b_favicon-32x32.png',
+        },
+      };
 
       const response = await fetch(webhook, {
         method: 'POST',
@@ -39,52 +50,15 @@ export default async function handler(req: any, res: any) {
         },
         body: JSON.stringify({
           content: null,
-          embeds: [
-            {
-              title: `Token Minted: ${token.content.metadata.name}`,
-              url: `https://solscan.io/token/${webhookData[0].events.tokenTransfers[0].token}`,
-              color: 16486972,
-              fields: [
-                {
-                  name: '\u200B',
-                  value: '\u200B',
-                },
-                {
-                  name: ':gem: Minted Amount',
-                  value: `**${webhookData[0].accountData.length} Tokens**`,
-                  inline: true,
-                },
-                {
-                  name: ':date: Mint Date',
-                  value: `<t:${webhookData[0].timestamp}:R>`,
-                  inline: true,
-                },
-                {
-                  name: '\u200B',
-                  value: '\u200B',
-                },
-                {
-                  name: 'Minting Account',
-                  value: webhookData[0].feePayer.slice(0, 4) + '..' + webhookData[0].feePayer.slice(-4),
-                  inline: true,
-                },
-              ],
-              image: {
-                url: token.content.files[0].uri,
-              },
-              timestamp: new Date().toISOString(),
-              footer: {
-                text: 'Helius',
-                icon_url: 'https://assets-global.website-files.com/641a8c4cac3aee8bd266fd58/642b5b2804ea37191a59737b_favicon-32x32.png',
-              },
-            },
-          ],
+          embeds: [embed],
         }),
       });
+
       console.log(response);
       res.status(200).json('success');
     }
   } catch (err) {
-    console.log(err);
+    console.error(err);
+    res.status(500).json('error');
   }
 }
