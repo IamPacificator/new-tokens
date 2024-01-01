694147faca94b7106c7567702fa1057d5ea71e46
IamPacificator <155380381+IamPacificator@users.noreply.github.com>
2024-01-01 22:00:41 +0400

    webhook.ts

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -1,4 +1,6 @@
-const rpc = `https://rpc.helius.xyz/?api-key=${process.env.HELIUS_KEY}`
+import fetch from 'node-fetch';
+
+const rpc = `https://rpc.helius.xyz/?api-key=${process.env.HELIUS_KEY}`;
 
 const getAsset = async (token: string) => {
   const response = await fetch(rpc, {
@@ -11,7 +13,7 @@ const getAsset = async (token: string) => {
       id: 'my-id',
       method: 'getAsset',
       params: {
-        id: token
+        id: token,
       },
     }),
   });
@@ -21,16 +23,14 @@ const getAsset = async (token: string) => {
 
 export default async function handler(req: any, res: any) {
   try {
-    if (req.method === "POST") {
+    if (req.method === 'POST') {
+      const webhook: any = process.env.DISCORD_WEBHOOK;
 
-      const webhook: any = process.env.DISCORD_WEBHOOK
+      let webhookData = req.body;
 
-      let webhook_data = req.body
-
-      console.log(webhook_data, 'e1')
-      console.log(webhook_data[0].events.nft)
-      console.log(webhook_data[0].events.nft.nfts[0])
-      let token: any = await getAsset(webhook_data[0].events.nft.nfts[0].mint)
+      console.log(webhookData, 'e1');
+      console.log(webhookData[0].events.tokenTransfers[0]);
+      let token: any = await getAsset(webhookData[0].events.tokenTransfers[0].token);
 
       const response = await fetch(webhook, {
         method: 'POST',
@@ -38,68 +38,53 @@ export default async function handler(req: any, res: any) {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-
-          "content": null,
-          "embeds": [
+          content: null,
+          embeds: [
             {
-              "title": token.content.metadata.name + " has sold!",
-              "url": `https://solscan.io/token/${webhook_data[0].events.nft.nfts[0].mint}`,
-              "color": 16486972,
-              "fields": [
+              title: `Token Minted: ${token.content.metadata.name}`,
+              url: `https://solscan.io/token/${webhookData[0].events.tokenTransfers[0].token}`,
+              color: 16486972,
+              fields: [
                 {
-                  "name": "\ ",
-                  "value": "\ "
+                  name: '\u200B',
+                  value: '\u200B',
                 },
                 {
-                  "name": "\ ",
-                  "value": "\ "
+                  name: ':gem: Minted Amount',
+                  value: `**${webhookData[0].accountData.length} Tokens**`,
+                  inline: true,
                 },
                 {
-                  "name": ":moneybag:  Sale Price",
-                  "value": "**" + (webhook_data[0].events.nft.amount / 1000000000).toFixed(2) + " " + "SOL**",
-                  "inline": true
+                  name: ':date: Mint Date',
+                  value: `<t:${webhookData[0].timestamp}:R>`,
+                  inline: true,
                 },
                 {
-                  "name": ":date:  Sale Date",
-                  "value": `<t:${webhook_data[0].timestamp}:R>`,
-                  "inline": true
+                  name: '\u200B',
+                  value: '\u200B',
                 },
                 {
-                  "name": "\ ",
-                  "value": "\ "
+                  name: 'Minting Account',
+                  value: webhookData[0].feePayer.slice(0, 4) + '..' + webhookData[0].feePayer.slice(-4),
+                  inline: true,
                 },
-                {
-                  "name": "Buyer",
-                  "value": webhook_data[0].events.nft.buyer.slice(0, 4) + '..' + webhook_data[0].events.nft.buyer.slice(-4),
-                  "inline": true
-                },
-                {
-                  "name": "Seller",
-                  "value": webhook_data[0].events.nft.seller.slice(0, 4) + '..' + webhook_data[0].events.nft.seller.slice(-4),
-                  "inline": true
-                }
               ],
-              "image": {
-                "url": token.content.files[0].uri
+              image: {
+                url: token.content.files[0].uri,
               },
               timestamp: new Date().toISOString(),
-              "footer": {
-                  "text": "Helius",
-                  "icon_url": "https://assets-global.website-files.com/641a8c4cac3aee8bd266fd58/642b5b2804ea37191a59737b_favicon-32x32.png",
-              }
-            }
+              footer: {
+                text: 'Helius',
+                icon_url: 'https://assets-global.website-files.com/641a8c4cac3aee8bd266fd58/642b5b2804ea37191a59737b_favicon-32x32.png',
+              },
+            },
           ],
-
-        },
-        ),
+        }),
       });
-      console.log(response)
-      res.status(200).json("success")
-
-    };
-
+      console.log(response);
+      res.status(200).json('success');
+    }
+  } catch (err) {
+    console.log(err);
   }
-
-  catch (err) { console.log(err) }
-
 }
